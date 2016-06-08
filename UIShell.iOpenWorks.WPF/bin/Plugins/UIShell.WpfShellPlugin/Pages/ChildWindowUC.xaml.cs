5a4b7be3dfa98ba6956cb95d7033fe369ac01f48
clumpy <howareyou_66@163.com>
2016-06-09 02:42:28 +0800

    增加文件

---
+++ UIShell.iOpenWorks.WPF/bin/Plugins/UIShell.WpfShellPlugin/Pages/ChildWindowUC.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIShell.WpfShellPlugin.Pages
+{
+    /// <summary>
+    /// ChildWindowUC.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChildWindowUC : UserControl
+    {
+        public event EventHandler Close;
+
+        public ChildWindowUC()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(sender, e);
+        }
+    }
+}
+
